3869972d0d2a77b98da422a5401daa1bc6fec333
tong <1972376180@qq.com>
2018-04-19 18:17:56 +0800

    init repository,and write some code about bubblesort

---
+++ src/main/java/com/tong/chapterThree/ArrayBub.java
@@ -0,0 +1,52 @@
+package com.tong.chapterThree;
+
+public class ArrayBub {
+
+    private long[] a;
+    private int nElems;
+
+    public ArrayBub(int max){
+        a = new long[max];
+        nElems = 0;
+    }
+
+    public void insert(long value){
+        a[nElems] = value;
+        nElems++;
+    }
+
+    public void display(){
+        if(nElems > 0){
+            for(int i = 0;i<nElems;i++){
+                System.out.println(a[i]+" ");
+            }
+        }else {
+            System.out.println(" ");
+        }
+    }
+
+
+    public void bubbleSort(){
+        int out,in;
+        for(out = nElems - 1;out > 1;out --){
+            for(in = 0;in < out;in++){
+                if(a[in]>a[in + 1]){
+                    swap(in ,in + 1);
+                }
+            }
+        }
+    }
+
+
+    private void swap(int in, int i) {
+        long temp = a[in];
+        a[in] = a[i];
+        a[i] = temp;
+
+    }
+
+
+
+
+
+}
---
+++ src/main/java/com/tong/chapterThree/BubbleSortApp.java
@@ -0,0 +1,26 @@
+package com.tong.chapterThree;
+
+public class BubbleSortApp {
+
+    public static void main(String[] args) {
+        int maxSize = 100;
+        ArrayBub arr;
+        arr = new ArrayBub(100);
+
+        arr.insert(29);
+        arr.insert(2);
+        arr.insert(298);
+        arr.insert(13);
+        arr.insert(34);
+        arr.insert(5);
+        arr.insert(9);
+        arr.insert(35);
+        arr.insert(47);
+        arr.insert(10);
+        arr.insert(100);
+        arr.insert(35);
+
+        arr.bubbleSort();
+        arr.display();
+    }
+}
