9890c4cd83ca659cc982e791fbf94e4fe80dffd1
tong <1972376180@qq.com>
2018-04-23 10:00:34 +0800

    使用隊列校驗分隔符

---
+++ src/main/java/com/tong/chapterFour/BracketChecker.java
@@ -0,0 +1,43 @@
+package com.tong.chapterFour;
+
+public class BracketChecker {
+    private String input;
+
+    public BracketChecker(String in){
+        input = in;
+    }
+
+    public void check(){
+        int stackSize = input.length();
+        CharStackX stack = new CharStackX(stackSize);
+        for(int j = 0;j<stackSize;j++){
+            char ch = input.charAt(j);
+            switch (ch){
+                case '{':
+                case '[':
+                case '(':
+                    stack.push(ch);
+                    break;
+
+                case '}':
+                case ']':
+                case ')':
+                    if(!stack.isEmpty()) {
+                        char chx = stack.pop();
+                        if ((ch == '}' && chx != '{') || (ch == ']' && chx != '[') || (ch == ')' && chx != '(')){
+                            System.out.println("Error:" + ch + "at " + j);
+                        }
+                    } else {
+                        System.out.println("Error:" + ch + " at" + j);
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        if(!stack.isEmpty())
+            System.out.println("Error:missing right delimeter");
+
+    }
+}
---
+++ src/main/java/com/tong/chapterFour/BracketsApp.java
@@ -0,0 +1,38 @@
+package com.tong.chapterFour;
+
+
+import com.sun.corba.se.spi.orbutil.fsm.Input;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BracketsApp {
+    public static void main(String[] args) {
+        String input ;
+        while (true){
+            System.out.println("Enter string containing delimeters:");
+            System.out.flush();
+            input = getString();
+            if (input.equals("")){
+                break;
+            }
+
+            BracketChecker checker = new BracketChecker(input);
+            checker.check();
+
+        }
+    }
+
+    public static String getString(){
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        String s = null;
+        try {
+            s = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return s;
+    }
+}
