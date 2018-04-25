b0425bc33a878dd915f46584767b78c282499cef
tong <1972376180@qq.com>
2018-04-25 10:06:14 +0800

    1，數組實現優先級隊列

---
+++ src/main/java/com/tong/chapterFour/priorityQueue/PriorityQApp.java
@@ -0,0 +1,20 @@
+package com.tong.chapterFour.priorityQueue;
+
+public class PriorityQApp {
+
+
+    public static void main(String[] args) {
+        PriorityQ thePQ = new PriorityQ(5);
+        thePQ.insert(30);
+        thePQ.insert(10);
+        thePQ.insert(50);
+        thePQ.insert(20);
+
+        while (!thePQ.isEmpty()){
+            long item = thePQ.remove();
+            System.out.println(item + " ");
+        }
+
+        System.out.println("");
+    }
+}
