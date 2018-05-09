7a8e2d579cd95271b8663465f1fdaed7dd40d9f9
tong <1972376180@qq.com>
2018-05-09 10:29:42 +0800

    1.三角數。2.變位字

---
+++ src/main/java/com/tong/lafore/chapterSix/AnagramApp.java
@@ -0,0 +1,77 @@
+package com.tong.lafore.chapterSix;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//變位字
+public class AnagramApp {
+
+    static int size;
+    static int count;
+    static char[] arrChar = new char[100];
+
+    public static void main(String[] args) throws IOException {
+        System.out.print("Enter a word : ");
+        String input = getString();
+        size = input.length();
+        count = 0;
+        for(int j = 0;j<size;j++){
+            arrChar[j] = input.charAt(j);
+        }
+        doAnagram(size);
+    }
+
+    public static String getString() throws IOException {
+        InputStreamReader reader = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(reader);
+        String s = br.readLine();
+        return s;
+    }
+
+    public static void doAnagram(int newsize){
+        if(newsize == 1 ){
+            return;
+        }
+        for(int j = 0;j<newsize;j++){
+            doAnagram(newsize - 1);
+            if(newsize == 2){
+                displayWord();
+            }
+            rotate(newsize);
+        }
+    }
+
+    public static void rotate(int newsize){
+        int j;
+        int position = size - newsize;
+        char temp = arrChar[position];
+        for(j = position + 1;j < size;j++){
+            arrChar[j-1] = arrChar[j];
+        }
+        arrChar[j-1] = temp;
+    }
+
+
+
+    public static void displayWord(){
+        if(count < 99){
+            System.out.print(" ");
+        }
+        if(count < 9){
+            System.out.print(" ");
+        }
+
+        System.out.print(++count + " ");
+        for(int j = 0;j < size;j++){
+            System.out.print(arrChar[j]);
+        }
+
+        System.out.print("  ");
+        System.out.print(" ");
+        System.out.flush();
+        if(count % 6 == 0){
+            System.out.println("");
+        }
+    }
+}
---
+++ src/main/java/com/tong/lafore/chapterSix/TriangleApp.java
@@ -0,0 +1,42 @@
+package com.tong.lafore.chapterSix;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//三角數字
+public class TriangleApp {
+    static int theNumber;
+
+    public static void main(String[] args) throws IOException {
+        System.out.print("Enter a Number : ");
+        theNumber = getInt();
+        int theAnswer = triangle(theNumber);
+        System.out.println("Triangle="+theAnswer);
+    }
+
+    public  static int triangle(int n){
+        System.out.println("Entering:n=" + n);
+        if(n==1){
+            System.out.println("Returning 1");
+            return 1;
+        }else {
+            int temp =  n + triangle(n-1);
+            System.out.println("Returning " + temp);
+            return temp;
+        }
+    }
+
+    public static String getString() throws IOException {
+        InputStreamReader reader = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(reader);
+        String s = br.readLine();
+        return s;
+    }
+
+    public static int getInt() throws IOException {
+        String s = getString();
+        return Integer.parseInt(s);
+    }
+}
