2a54ff2048ee10ff07c711e23507326db3848a41
tong <1972376180@qq.com>
2018-05-10 10:36:45 +0800

    1.遞歸二分查找

---
+++ src/main/java/com/tong/lafore/chapterSix/BinarySerachApp.java
@@ -0,0 +1,35 @@
+package com.tong.lafore.chapterSix;
+
+public class BinarySerachApp {
+
+    public static void main(String[] args) {
+        int maxSize = 100;
+        OrdArray arr;
+        arr = new OrdArray(maxSize);
+
+        arr.insert(72);
+        arr.insert(90);
+        arr.insert(45);
+        arr.insert(126);
+        arr.insert(54);
+        arr.insert(99);
+        arr.insert(144);
+        arr.insert(27);
+        arr.insert(135);
+        arr.insert(81);
+        arr.insert(18);
+        arr.insert(108);
+        arr.insert(9);
+        arr.insert(117);
+        arr.insert(63);
+        arr.insert(36);
+        arr.display();
+
+        int searchKey = 27;
+        if(arr.find(searchKey)!= arr.size()){
+            System.out.println("Found " + searchKey);
+        }else {
+            System.out.println("Can't find " + searchKey);
+        }
+    }
+}
