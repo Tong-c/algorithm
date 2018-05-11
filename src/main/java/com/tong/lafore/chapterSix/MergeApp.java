9a245b9002c10a78bd1aa6c0063da6bba8255360
tong <1972376180@qq.com>
2018-05-11 10:18:27 +0800

    1.漢諾塔。2.歸併排序

---
+++ src/main/java/com/tong/lafore/chapterSix/MergeApp.java
@@ -0,0 +1,46 @@
+package com.tong.lafore.chapterSix;
+
+import static com.sun.glass.ui.gtk.GtkApplication.display;
+import static com.sun.glass.ui.gtk.GtkApplication.display;
+
+//歸并排序
+public class MergeApp {
+
+    public static void main(String[] args) {
+        int[] arrayA = {23,47,81,95};
+        int[] arrayB = {7,14,39,55,62,74};
+        int[] arrayC = new int[10];
+
+        merge(arrayA,4,arrayB,6,arrayC);
+        display(arrayC,10);
+        
+    }
+
+
+    private static void display(int[] theArray, int size) {
+        for(int j = 0;j<size;j++){
+            System.out.print(theArray[j] + " ");
+        }
+        System.out.println("");
+    }
+
+
+    private static void merge(int[] arrayA, int sizeA, int[] arrayB, int sizeB, int[] arrayC) {
+        int aDex = 0,bDex = 0,cDex = 0;
+        while (aDex < sizeA && bDex < sizeB){
+            if(arrayA[aDex] < arrayB[bDex]){
+                arrayC[cDex++] = arrayA[aDex++];
+            }else {
+                arrayC[cDex++] = arrayB[bDex++];
+            }
+        }
+
+        while (aDex < sizeA){
+            arrayC[cDex++] = arrayA[aDex++];
+        }
+
+        while (bDex < sizeB){
+            arrayC[cDex++] = arrayB[bDex++];
+        }
+    }
+}
---
+++ src/main/java/com/tong/lafore/chapterSix/TowersApp.java
@@ -0,0 +1,22 @@
+package com.tong.lafore.chapterSix;
+
+//漢諾塔
+public class TowersApp {
+
+    static int nDisks = 3;
+
+    public static void main(String[] args) {
+        doTowers(nDisks,'A','B','C');
+    }
+
+    public static void doTowers(int topN,char from,char inter,char to){
+        if(topN == 1){
+            System.out.println("Disk 1 from " + from + " to " + to);
+        }else {
+            doTowers(topN -1 ,from,to,inter);
+            System.out.println("Disk " + topN + " from " + from + " to " + to);
+            doTowers(topN - 1,inter,from,to);
+        }
+
+    }
+}
