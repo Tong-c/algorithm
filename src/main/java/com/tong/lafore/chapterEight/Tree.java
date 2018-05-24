1f399687452923081893fcc9951b0aeaf60913bc
tong <1972376180@qq.com>
2018-05-24 11:11:46 +0800

    1.二叉樹

--- src/main/java/com/tong/lafore/chapterEight/Tree.java
+++ src/main/java/com/tong/lafore/chapterEight/Tree.java
@@ -1,22 +1,24 @@
 package com.tong.lafore.chapterEight;
 
+import java.util.Stack;
+
 public class Tree {
 
     private Node root;
 
-    public Tree(){
+    public Tree() {
         root = null;
     }
 
-    public Node find(int key){
+    public Node find(int key) {
         Node current = root;
-        while (current.iData != key){
-            if(key < current.iData){
+        while (current.iData != key) {
+            if (key < current.iData) {
                 current = current.leftChild;
             } else {
                 current = current.rightChild;
             }
-            if(current == null){
+            if (current == null) {
                 return null;
             }
 
@@ -25,27 +27,27 @@ public class Tree {
     }
 
 
-    public void insert(int id ,double dd){
+    public void insert(int id, double dd) {
         Node newNode = new Node();
         newNode.iData = id;
         newNode.dData = dd;
 
-        if(root == null){
+        if (root == null) {
             root = newNode;
         } else {
             Node current = root;
             Node parent;
-            while (true){
+            while (true) {
                 parent = current;
-                if(id < current.iData){
+                if (id < current.iData) {
                     current = current.leftChild;
-                    if(current == null){
+                    if (current == null) {
                         parent.leftChild = newNode;
                         return;
                     }
-                }else {
+                } else {
                     current = current.rightChild;
-                    if(current == null){
+                    if (current == null) {
                         parent.rightChild = newNode;
                         return;
                     }
@@ -55,7 +57,7 @@ public class Tree {
     }
 
 
-    public boolean delete(int key){
+    public boolean delete(int key) {
         Node current = root;
         Node parent = root;
         boolean isLeftChild = true;
@@ -73,59 +75,150 @@ public class Tree {
                 return false;
             }
         }
-            if (current.leftChild == null && current.rightChild == null){
-                if(current == root){
-                    root = null;
-                }else if(isLeftChild){
-                    parent.leftChild = null;
-                }else {
-                    parent.rightChild = null;
-                }
-            }else if(current.rightChild == null){
-                if(current == root){
-                    root = current.leftChild;
-                }else if(isLeftChild){
-                    parent.leftChild = current.leftChild;
-                }else {
-                    parent.rightChild = current.leftChild;
-                }
-            }else if(current.leftChild == null){
-                if(current == root){
-                    root = current.rightChild;
-                }else if(isLeftChild){
-                    parent.leftChild = current.rightChild;
-                }else {
-                    parent.rightChild = current.rightChild;
-                }
-            }else {
-                Node successor = getSuccessor(current);
-                if(current == root){
-                    root = successor;
-                } else if(isLeftChild){
-                    parent.leftChild = successor;
-                }else {
-                    parent.rightChild = successor;
-                }
-                successor.leftChild = current.leftChild;
+        if (current.leftChild == null && current.rightChild == null) {
+            if (current == root) {
+                root = null;
+            } else if (isLeftChild) {
+                parent.leftChild = null;
+            } else {
+                parent.rightChild = null;
             }
-            return true;
+        } else if (current.rightChild == null) {
+            if (current == root) {
+                root = current.leftChild;
+            } else if (isLeftChild) {
+                parent.leftChild = current.leftChild;
+            } else {
+                parent.rightChild = current.leftChild;
+            }
+        } else if (current.leftChild == null) {
+            if (current == root) {
+                root = current.rightChild;
+            } else if (isLeftChild) {
+                parent.leftChild = current.rightChild;
+            } else {
+                parent.rightChild = current.rightChild;
+            }
+        } else {
+            Node successor = getSuccessor(current);
+            if (current == root) {
+                root = successor;
+            } else if (isLeftChild) {
+                parent.leftChild = successor;
+            } else {
+                parent.rightChild = successor;
+            }
+            successor.leftChild = current.leftChild;
+        }
+        return true;
     }
+
 
     private Node getSuccessor(Node delNode) {
         Node successorParent = delNode;
         Node successor = delNode;
         Node current = delNode.rightChild;
-        while (current != null){
+        while (current != null) {
             successorParent = successor;
             successor = current;
             current = current.leftChild;
         }
 
-        if(successor != delNode.rightChild){
+        if (successor != delNode.rightChild) {
             successorParent.leftChild = successor.rightChild;
             successor.rightChild = delNode.rightChild;
         }
 
         return successor;
+    }
+
+    public void traverse(int traverseType) {
+        switch (traverseType) {
+            case 1:
+                System.out.print("\nPreorder traversal: ");
+                preOrder(root);
+                break;
+            case 2:
+                System.out.print("\nInorder traversal: ");
+                inOrder(root);
+                break;
+            case 3:
+                System.out.print("\nPostorder traversal: ");
+                postOrder(root);
+                break;
+        }
+        System.out.println();
+    }
+
+    private void postOrder(Node localRoot) {
+        if (localRoot != null) {
+            postOrder(localRoot.leftChild);
+            postOrder(localRoot.rightChild);
+            System.out.print(localRoot.iData + " ");
+        }
+    }
+
+    private void inOrder(Node localRoot) {
+        if (localRoot != null) {
+            inOrder(localRoot.leftChild);
+            System.out.print(localRoot.iData + " ");
+            inOrder(localRoot.rightChild);
+        }
+    }
+
+    private void preOrder(Node localRoot) {
+        if (localRoot != null) {
+            System.out.print(localRoot.iData + " ");
+            preOrder(localRoot.leftChild);
+            preOrder(localRoot.rightChild);
+        }
+    }
+
+
+    public void displayTree() {
+        Stack globalStack = new Stack();
+        globalStack.push(root);
+        int nBlanks = 32;
+        boolean isRowEmpty = false;
+        System.out.println("...........................");
+        while (isRowEmpty == false) {
+            Stack localStack = new Stack();
+            isRowEmpty = true;
+            for (int j = 0; j < nBlanks; j++) {
+                System.out.print(' ');
+            }
+
+            while (globalStack.isEmpty() == false) {
+                Node temp = (Node) globalStack.pop();
+                if (temp != null) {
+                    System.out.print(temp.iData);
+                    localStack.push(temp.leftChild);
+                    localStack.push(temp.rightChild);
+
+                    if (temp.leftChild != null || temp.rightChild != null) {
+                        isRowEmpty = false;
+                    }
+
+
+                } else {
+                    System.out.print("..");
+                    localStack.push(null);
+                    localStack.push(null);
+                }
+                for (int j = 0; j < nBlanks * 2 - 2; j++) {
+                    System.out.print(' ');
+                }
+            }
+
+            System.out.println();
+            nBlanks /= 2;
+            while (localStack.isEmpty() == false){
+                globalStack.push(localStack.pop());
+            }
+
+
+        }
+
+        System.out.println("...........................");
     }
 }
