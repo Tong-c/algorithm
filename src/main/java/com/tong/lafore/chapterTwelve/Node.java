ff90f45e5eee396337dbbd4488d038a737a03da5
tong <1972376180@qq.com>
2018-06-06 10:24:35 +0800

    1.堆一

---
+++ src/main/java/com/tong/lafore/chapterTwelve/Node.java
@@ -0,0 +1,18 @@
+package com.tong.lafore.chapterTwelve;
+
+public class Node {
+
+    private int iData;
+
+    public Node(int key){
+        iData = key;
+    }
+
+    public int getKey(){
+        return iData;
+    }
+
+    public void setKey(int id){
+        iData = id;
+    }
+}
