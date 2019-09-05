4532b0c33559a97456e32d8ea58c7c64693c1d08
Tong-c <1972376180@qq.com>
2019-09-05 18:48:19 +0800

    1.归并排序

--- src/main/java/com/tong/wangzheng/sort/Sort.java
+++ src/main/java/com/tong/wangzheng/sort/Sort.java
@@ -1,7 +1,5 @@
 package com.tong.wangzheng.sort;
 
-import com.tong.wangzheng.queue.ArrayQueue;
-
 import java.util.Arrays;
 
 /**
@@ -40,6 +38,7 @@ public class Sort {
      */
     public void insertSort(int[] nums) {
         int length = nums.length;
+
         if (length <= 1) {
             System.out.println(Arrays.toString(nums));
             return;
@@ -84,6 +83,56 @@ public class Sort {
     }
 
 
+    /**
+     * 归并排序
+     */
+    public void mergeSort(int[] nums) {
+        int length = nums.length;
+        int[] workSpace = new int[length];
+        recMergeSort(nums, workSpace, 0, length - 1);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    private void recMergeSort(int[] nums, int[] workSpace, int lowerBound, int upperBound) {
+        if (lowerBound == upperBound) {
+            return;
+        } else {
+            int mid = (lowerBound + upperBound) / 2;
+            recMergeSort(nums, workSpace, lowerBound, mid);
+            recMergeSort(nums, workSpace, mid + 1, upperBound);
+            merge(nums, workSpace, lowerBound, mid + 1, upperBound);
+        }
+    }
+
+    private void merge(int[] nums, int[] workSpace, int lowPtr, int highPtr, int upperBound) {
+        int j = 0;
+        int lowerBound = lowPtr;
+        int mid = highPtr - 1;
+        int n = upperBound - lowerBound + 1;
+
+        while (lowPtr <= mid && highPtr <= upperBound) {
+            if (nums[lowerBound] < nums[highPtr]) {
+                workSpace[j++] = nums[lowPtr++];
+            } else {
+                workSpace[j++] = nums[highPtr++];
+            }
+        }
+
+        while (lowPtr <= mid) {
+            workSpace[j++] = nums[lowPtr++];
+        }
+
+        while (highPtr <= upperBound) {
+            workSpace[j++] = nums[highPtr++];
+        }
+
+        for (j = 0; j < n; j++) {
+            nums[lowerBound + j] = workSpace[j];
+        }
+
+    }
+
+
     private void swap(int[] nums, int index1, int index2) {
         int temp = nums[index1];
         nums[index1] = nums[index2];
@@ -91,9 +140,115 @@ public class Sort {
     }
 
 
+    /**
+     * 归并排序 2
+     */
+    public void mergeSort2(int[] nums) {
+        int length = nums.length;
+        if (length <= 1) {
+            System.out.println(Arrays.toString(nums));
+            return;
+        }
+        int[] workspace = new int[length];
+        recMergeSort2(nums, workspace, 0, length - 1);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    private void recMergeSort2(int[] nums, int[] workspace, int lowerBound, int upperBound) {
+        if (upperBound <= lowerBound) {
+            return;
+        }
+        int mid = (upperBound + lowerBound) / 2;
+        recMergeSort2(nums, workspace, lowerBound, mid);
+        recMergeSort2(nums, workspace, mid + 1, upperBound);
+        merge2(nums, workspace, lowerBound, mid, upperBound);
+    }
+
+    private void merge2(int[] nums, int[] workspace, int lowerBound, int mid, int upperBound) {
+        int point1 = lowerBound;
+        int point2 = mid + 1;
+        int i = 0;
+        while (point1 <= mid && point2 <= upperBound) {
+            if (nums[point1] <= nums[point2]) {
+                workspace[i++] = nums[point1];
+                point1++;
+            } else {
+                workspace[i++] = nums[point2];
+                point2++;
+            }
+        }
+
+        while (point1 <= mid) {
+            workspace[i++] = nums[point1];
+            point1++;
+        }
+
+        while (point2 <= upperBound) {
+            workspace[i++] = nums[point2];
+            point2++;
+        }
+
+        for (int x = 0; x < i; x++) {
+            nums[lowerBound + x] = workspace[x];
+        }
+    }
+
+    /**
+     * 归并排序 3
+     *
+     * @param nums
+     */
+    public void mergeSort3(int[] nums) {
+        int length = nums.length;
+        if (length <= 1) {
+            System.out.println(Arrays.toString(nums));
+        }
+
+        int[] temp = new int[length];
+        recMergeSort3(nums, temp, 0, length - 1);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    private void recMergeSort3(int[] nums, int[] temp, int left, int right) {
+        if (right <= left) {
+            return;
+        }
+
+        int middle = (left + right) / 2;
+        recMergeSort3(nums, temp, left, middle);
+        recMergeSort3(nums, temp, middle + 1, right);
+        merge3(nums, temp, left, middle + 1, right);
+    }
+
+    private void merge3(int[] nums, int[] temp, int lowerBound, int middle, int upperBound) {
+        int leftPointer = lowerBound;
+        int rightPointer = middle;
+        int i = 0;
+        while (leftPointer <= middle - 1 && rightPointer <= upperBound) {
+            if (nums[leftPointer] <= nums[rightPointer]) {
+                temp[i++] = nums[leftPointer++];
+            } else {
+                temp[i++] = nums[rightPointer++];
+            }
+        }
+
+        while (leftPointer <= middle - 1) {
+            temp[i++] = nums[leftPointer++];
+        }
+
+        while (rightPointer <= upperBound) {
+            temp[i++] = nums[rightPointer++];
+        }
+
+        for (int j = 0; j < i; j++) {
+            nums[lowerBound + j] = temp[j];
+        }
+    }
+
+
     public static void main(String[] args) {
-        int[] data = {3, 2, 1, 10, 4, 5};
+        int[] data = {3, 2, 1, 10, 4, 5, 4, 11, 8, 1, 8, 3};
         Sort sort = new Sort();
-        sort.selectSort(data);
+        sort.mergeSort3(data);
     }
 }
