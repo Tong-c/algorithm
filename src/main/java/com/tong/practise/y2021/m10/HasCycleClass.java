7c009a6f72bb713305564ed2ef0b1f73481643ff
tongchuang <tongchuang@qdama.cn>
2021-11-01 10:14:36 +0800

    环形链表 - 快慢双指针

---
+++ src/main/java/com/tong/practise/y2021/m10/HasCycleClass.java
@@ -0,0 +1,46 @@
+package com.tong.practise.y2021.m10;
+
+import com.tong.practise.y2021.m03.ListNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HasCycleClass {
+
+    public boolean hasCycle(ListNode head) {
+        if (null == head || null == head.next) {
+            return false;
+        }
+        List<ListNode> nodes = new ArrayList<>();
+        ListNode pointer = head;
+        while (null != pointer) {
+            nodes.add(pointer);
+            pointer = pointer.next;
+            if (nodes.contains(pointer)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean hasCycle2(ListNode head) {
+        if (null == head || null == head.next) {
+            return false;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        while (null != fast && null != fast.next) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
