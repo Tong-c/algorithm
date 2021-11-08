a65845459900b85992788ad278caecf0187158e2
tongchuang <1972376180@qq.com>
2021-11-08 13:53:02 +0800

    岛屿数量

---
+++ src/main/java/com/tong/practise/y2021/m11/NumIslands.java
@@ -0,0 +1,36 @@
+package com.tong.practise.y2021.m11;
+
+public class NumIslands {
+    private int res;
+
+    public int numIslands(char[][] grid) {
+        res = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == '1') {
+                    dfsGrid(grid, i, j);
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+
+
+    private void dfsGrid(char[][] grid, int row, int col) {
+        if (row >= grid.length || col >= grid[0].length || row < 0 || col < 0) {
+            return;
+        }
+
+        if (grid[row][col] != '1') {
+            return;
+        }
+
+        grid[row][col] = '2';
+        dfsGrid(grid, row - 1, col);
+        dfsGrid(grid, row + 1, col);
+        dfsGrid(grid, row, col - 1);
+        dfsGrid(grid, row, col + 1);
+    }
+
+}
---
+++ src/main/java/com/tong/practise/y2021/m11/NumsIsland2.java
@@ -0,0 +1,34 @@
+package com.tong.practise.y2021.m11;
+
+public class NumsIsland2 {
+
+    public int numIslands(char[][] grid) {
+        int res = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == '1') {
+                    dfsGrid(grid, i, j);
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+
+    private void dfsGrid(char[][] grid, int row, int col) {
+        if (row >= grid.length || col >= grid[0].length || row < 0 || col < 0) {
+            return;
+        }
+
+        if (grid[row][col] != '1') {
+            return;
+        }
+
+        grid[row][col] = '2';
+        dfsGrid(grid, row + 1, col);
+        dfsGrid(grid, row - 1, col);
+        dfsGrid(grid, row, col - 1);
+        dfsGrid(grid, row, col + 1);
+    }
+
+}
