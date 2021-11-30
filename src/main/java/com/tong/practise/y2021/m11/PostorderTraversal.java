488b736042a7f248580b0ed5ae8170b4c9869178
tongchuang <1972376180@qq.com>
2021-11-30 10:13:01 +0800

    二叉树的后序遍历

---
+++ src/main/java/com/tong/practise/y2021/m11/PostorderTraversal.java
@@ -0,0 +1,37 @@
+package com.tong.practise.y2021.m11;
+
+import com.tong.practise.y2021.m03.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class PostorderTraversal {
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> nodeVals = new ArrayList<>();
+        if (null == root) {
+            return nodeVals;
+        }
+
+        Stack<TreeNode> stack1 = new Stack<>();
+        Stack<TreeNode> stack2 = new Stack<>();
+        stack1.push(root);
+        while (!stack1.isEmpty()) {
+            TreeNode node = stack1.pop();
+            stack2.push(node);
+            if (node.left != null) {
+                stack1.push(node.left);
+            }
+            if (node.right != null) {
+                stack1.push(node.right);
+            }
+        }
+
+        while (!stack2.isEmpty()) {
+            nodeVals.add(stack2.pop().val);
+        }
+
+        return nodeVals;
+    }
+}
