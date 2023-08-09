c6a2ba314be38f6c5d269ac375264c659e126136
tc <1972376180@qq.com>
2023-08-09 17:53:31 +0800

    最长回文子串

---
+++ src/main/java/com/tong/practise/y2023/array/LongestPalindrome.java
@@ -0,0 +1,16 @@
+package com.tong.practise.y2023.array;
+
+public class LongestPalindrome {
+
+    public String longestPalindrome(String s) {
+        return "";
+    }
+
+    public static void main(String[] args) {
+        LongestPalindrome longestPalindrome = new LongestPalindrome();
+        String s = "babad";
+        // bab
+        System.out.println(longestPalindrome.longestPalindrome(s));
+    }
+
+}
