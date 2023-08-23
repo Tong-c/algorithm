6f6fe07b0e02f4b24390da66c76b4e4e03de1794
tongchuang <tongchuang@qdama.cn>
2023-08-23 15:13:56 +0800

    字符串的排列

---
+++ src/main/java/com/tong/practise/y2023/string/CheckInclusion.java
@@ -0,0 +1,42 @@
+package com.tong.practise.y2023.string;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CheckInclusion {
+
+    public boolean checkInclusion(String s1, String s2) {
+        Map<Character, Integer> ori = new HashMap<>();
+        for (int i = 0; i < s1.length(); i++) {
+            ori.put(s1.charAt(i), ori.getOrDefault(s1.charAt(i), 0) + 1);
+        }
+
+        Map<Character, Integer> window = new HashMap<>();
+        int l = 0;
+        int r = 0;
+        int valid = 0;
+
+        while (r < s2.length()) {
+            char sChar = s2.charAt(r);
+            r++;
+            if (ori.containsKey(sChar)) {
+                window.put(sChar, window.getOrDefault(sChar, 0) + 1);
+                if (window.get(sChar).equals(ori.get(sChar))) {
+                    valid++;
+                    if (valid == ori.size()) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        CheckInclusion checkInclusion = new CheckInclusion();
+        String s1 = "";
+        String s2 = "eldbaooo";
+        boolean result = checkInclusion.checkInclusion(s1, s2);
+        System.out.println(result);
+    }
+}
