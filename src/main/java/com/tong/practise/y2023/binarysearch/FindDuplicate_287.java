8772032e2ecff610552a3ec690f4183f5b796784
tongchuang <tongchuang@qdama.cn>
2023-10-13 11:00:23 +0800

    寻找重复数

--- src/main/java/com/tong/practise/y2023/binarysearch/FindDuplicate_287.java
+++ src/main/java/com/tong/practise/y2023/binarysearch/FindDuplicate_287.java
@@ -3,45 +3,23 @@ package com.tong.practise.y2023.binarysearch;
 public class FindDuplicate_287 {
 
     public int findDuplicate(int[] nums) {
-        int size = nums.length;
-        int result = doFind(nums, 0, size - 1);
-        if (result >= 0) {
-            return nums[result];
-        }
-        return -1;
-    }
-
-    private int doFind(int[] nums, int startIndex, int endIndex) {
-        if (startIndex == endIndex) {
-            return -1;
-        }
-        if (startIndex + 1 == endIndex) {
-            if (nums[startIndex] == nums[endIndex]) {
-                return startIndex;
-            } else {
-                return -1;
-            }
-        }
-
-        int mid = startIndex + (endIndex - startIndex) / 2;
-        int leftFindResult = doFind(nums, startIndex, mid);
-        int rightFindResult = doFind(nums, mid + 1, endIndex);
-        if (leftFindResult >= 0) {
-            return leftFindResult;
-        }
-        if (rightFindResult >= 0) {
-            return rightFindResult;
-        }
-        for (int i = startIndex; i <= mid; i++) {
-            int leftNum = nums[i];
-            for (int j = mid + 1; j <= endIndex; j++) {
-                int rightNum = nums[j];
-                if (leftNum == rightNum) {
-                    return i;
+        int low = 1;
+        int high = nums.length - 1;
+        while (low < high) {
+            int mid = (high + low) / 2;
+            int count = 0;
+            for (int num : nums) {
+                if (num <= mid) {
+                    count++;
                 }
             }
+            if (count <= mid) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
         }
-        return -1;
+        return low;
     }
 
     public static void main(String[] args) {
